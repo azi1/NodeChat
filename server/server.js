a961de39442c7ad7a748b5c425292071452ce85a
azi1 <azher1_zahid@hotmail.com>
2018-10-23 20:28:19 +0400

    sockets created

--- server/server.js
+++ server/server.js
@@ -1,13 +1,35 @@
-
+require('../config/config');
 const path = require('path');
 const express = require('express');
-require('../config/config');
+const http = require('http');
+const socketIO = require('socket.io');
+
 
 const publicPath = path.join(__dirname, '../public');
 console.log(publicPath);
 const port  = process.env.PORT;
 const app = express();
-app.use(express.static(publicPath));
+const server = http.createServer(app);
+const io = socketIO(server);
+app.use(express.static(publicPath)); // middleware
+
+io.on('connection', (socket)=>{
+    console.log('new user connected');
+
+    socket.on('createMessage', (message) => {
+        console.log(message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+    });
+ 
+
+    socket.on('disconnect', () => {
+        console.log('client disconnected');
+    });
+});
 
 
 
@@ -16,6 +38,8 @@ app.use(express.static(publicPath));
 
 
 
-app.listen(port,()=>{
+
+
+server.listen(port,()=>{
     console.log(`SERVER is up on ${port}`);
 });
